c018504d3a1d6e3392ea11514b93f9b9cebe63d9
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2021-07-14 23:13:55 +0800

    plotly was commented out. fix it.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,53 @@
+// jshint esversion: 6, node: true
+
 import $ from 'jquery';
 import dt from 'datatables.net';
 import 'datatables.net-dt/css/jquery.dataTables.css';
-import csvPath from './assets/ciafb.csv';
 import Papa from 'papaparse';
+import Plotly from 'plotly.js-basic-dist-min';
 
-$.get(csvPath, main);
+import ciafbPath from './assets/ciafb.csv';
+
+$.get(ciafbPath, main);
 
 function main(ciafb) {
-    ciafb = Papa.parse(ciafb).data;
-    console.log(ciafb);
-    ciafb = ciafb.slice(1).filter(x => x.length>=7);
-    console.log(ciafb);
-    let i;
-    ciafb = ciafb.map(function(fields) {
-	for (i of [1, 5, 6]) {
-	    fields[i] = parseInt(fields[i]);
-	}
-	for (i of [2, 3, 4]) {
-	    fields[i] = parseFloat(fields[i]);
-	}
-	return fields;
+    ciafb = Papa.parse(ciafb, {
+      header: true,
+      dynamicTyping: true,
+      skipEmptyLines: 'greedy',
+    } ).data;
+    console.log('ciafb full data:', ciafb);
+    for (let entry of ciafb) {
+      for (let col in entry) {
+        if (col != 'Country' && typeof entry[col] != 'number')
+	  entry[col] = NaN;
+      }
+    }
+    let cols = Object.keys(ciafb[0]);
+    cols = cols.map(k => ( {
+      data: k,
+      title: k,
+      type: 'num',
+    }));
+    cols[0] = Object.assign(cols[0], {
+      type: 'string',
     });
+    console.log('cols:', cols);
     $('#ciafb_table').DataTable({
 	data: ciafb,
-	fixedHeader: true,
 	paging: false,
-	columns: [
-	    { title: 'Country' },
-	    { title: 'Population' },
-	    { title: 'Median age' },
-	    { title: 'Population growth rate' },
-	    { title: 'Birth rate' },
-	    { title: 'Telephones - mobile cellular' },
-	    { title: 'Internet users' },
-	]
+	columns: cols,
     });
+
     let canvas = $('#canvas')[0];
-    let markerSize = ciafb.map(country => Math.sqrt(country[1])*0.005);
-    console.log(markerSize);
-    /*
+    let markerSize = ciafb.map(entry => Math.sqrt(entry.Population)*0.005);
+    console.log('markerSize:', markerSize);
     Plotly.newPlot(canvas, [ {
       type: 'scatter',
       mode: 'markers',
-      x: ciafb.map(country => country[6]/country[1]),
-      y: ciafb.map(country => country[5]/country[1]),
+      x: ciafb.map(entry => entry.Internet_users/entry.Population),
+      y: ciafb.map(entry => entry.Phones_mobile/entry.Population),
+      text: ciafb.map(entry => entry.Country),
       marker: {
 	color: 'rgba(255,255,255,0.3)',
 	size: markerSize,
@@ -52,15 +56,17 @@ function main(ciafb) {
 	  width: 2
 	}
       },
-      text: ciafb.map(country => country[0])
     }], {
       xaxis: {
-	type: 'linear'
+	title: {
+	  text: 'Internet users / Population'
+	}
       },
       yaxis: {
-	type: 'linear'
+	title: {
+	  text: 'Phones (mobile) / Population'
+	}
       },
       hovermode: 'closest',
     });
-    */
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
+// https://alligator.io/tooling/webpack-gulp-grunt-browserify/
+// What Tool to Use: webpack vs Gulp vs Grunt vs Browserify
+// https://www.valentinog.com/blog/webpack/
+// A mostly complete guide to webpack 5 (2020)
+//
+// https://webpack.js.org/guides/getting-started/
+// webpack Getting Started
+//
+// https://gist.github.com/marcstober/c34bb4bdf7ef622cb24d6675723749bd
+// the simplest way to make jQuery DataTables work with npm and webpack
+// https://gist.github.com/jrunestone/2fbe5d6d5e425b7c046168b6d6e74e95
+// slightly more complicated way
+// https://www.npmjs.com/package/datatables
+//
+// https://neighborhood999.github.io/webpack-tutorial-gitbook/Part1/
+// Webpack 簡介
+// https://hsiangfeng.github.io/categories/webpack/
+// Webpack 5 走起!
+// https://ithelp.ithome.com.tw/users/20069901/ironman/1074
+// 使用 webpack 模組化你的程式碼，讓人生更美好
+//
+//
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
